bd23a0cfd687bf71f5433cc3d66969ac59844cac
Gustavo Drumond <gdrumondduarte@gmail.com>
2024-12-03 09:12:03 -0300

    Terminado o corpo do componente e estilizando o mesmo

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,38 @@
 import { FaUser, FaLock } from 'react-icons/fa'
+import { useState } from 'react'
+import './Login.css'
 
 const Login = () => {
+
+    const [userEmail, setUserEmail] = useState('')
+    const [userPassword, setUserPassword] = useState('')
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log("Formulario enviado com sucesso")
+
+        console.log("Daddos do formulario: " + userEmail + " - " + userPassword)
+    }
+
     return (
-        <div>
-            <form>
+        <div className='login-container'>
+            <form onSubmit={handleLogin}>
                 <h3>Acesse o sistema</h3>
-                <div>
+                <div className='input-field' >
                     <input
                         type="email"
                         name="email"
                         placeholder="Email"
+                        onChange={(e) => setUserEmail(e.target.value)}
                     />
                     <FaUser className='icon'/>
                 </div>
-                <div>
+                <div className='input-field'>
                     <input
                         type="password"
                         name="password"
                         placeholder="Senha"
+                        onChange={(e) => setUserPassword(e.target.value)}
                     />
                     <FaLock className='icon'/>
                 </div>
